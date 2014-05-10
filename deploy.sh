402eaf25f44158bdb38d397e1c60b4edbeb0eeb3
Aris Samad-Yahaya <aris@quickschools.com>
2014-05-10 12:55:12 -0700

    Test again

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo Commit message: $1
+
+echo -e "\033[1m--Incrementing version\033[0m"
+mvn autoincrement-versions:increment
+
+echo -e "\033[1m--Committing to Github\033[0m"
+git add .
+git add -u
+git commit -m $1
+git push
+
+echo -e "\033[1m--Deploying\033[0m"
+mvn deploy
